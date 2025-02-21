029adc7f91051e50df980fe5984604cbfa87899a
VetriRex <kvetri944@gmail.com>
2025-02-21 12:05:42 +0530

    fixed some bugs

--- ems-frontend/src/components/FooterComponent.jsx
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -4,7 +4,17 @@ import React from 'react'
 const FooterComponent = () => {
   return (
     <div>
-        <footer className='footer' style={{position: "fixed", bottom: "0", width: "100%", height: "50px", color: "white", backgroundColor:"black",textAlign:"center"}}>
+        <footer className='footer' style={{
+          
+          position: "fixed", 
+          bottom: "0", 
+          width: "100%", 
+          height: "50px", 
+          color: "white", 
+          backgroundColor:"black",
+          textAlign:"center"
+          
+          }}>
             <span>All rights reserved 2023 by vleven</span>
         </footer>
     </div>
